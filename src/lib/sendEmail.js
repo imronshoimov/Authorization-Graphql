227865b136e1d599d2354a61404c2e0aa84777ad
imronshoimov <imronshoimov125@gmail.com>
2021-08-16 10:28:03 +0500

    sendCode func

--- src/lib/sendEmail.js
+++ src/lib/sendEmail.js
@@ -1,26 +1,30 @@
 import nodemailer from 'nodemailer'
 
-const transporter = nodemailer.createTransport({
-    service: 'mail.ru',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'imronshoimov@mail.ru',
-        pass: 'ZBMfQPR7tvy1VS0AB0Vb'
+function sendCode (email, code) {
+    const transporter = nodemailer.createTransport({
+        service: 'mail.ru',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'imronshoimov@mail.ru',
+            pass: 'ZBMfQPR7tvy1VS0AB0Vb'
+        }
+    })
+    
+    const mailOptions = {
+        from: 'imronshoimov@mail.ru',
+        to: email,
+        subject: 'Verification code',
+        html: '<p>your code: ' + code + '</p>'
     }
-})
-
-const mailOptions = {
-    from: 'imronshoimov@mail.ru',
-    to: 'imronshoimov125@gmail.com',
-    subject: 'Verification code',
-    html: '<h1>Hello World</h1>'
+    
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(info);
+        }
+    })
 }
 
-transporter.sendMail(mailOptions, (err, info) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(info);
-    }
-})
+export default sendCode
